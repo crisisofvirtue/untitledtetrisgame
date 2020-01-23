4fe4816192e9cf6a0c84e303ce3b9520ccc678e1
crisisofvirtue <crisisofvirtue@gmail.com>
2020-01-23 08:44:35 -0500

    Adding empty 2d sidescroller project

---
+++ UnrealProject/UntitledTetris/Intermediate/Build/Win64/UE4Editor/Inc/UntitledTetris/UntitledTetrisGameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNTITLEDTETRIS_UntitledTetrisGameMode_generated_h
+#error "UntitledTetrisGameMode.generated.h already included, missing '#pragma once' in UntitledTetrisGameMode.h"
+#endif
+#define UNTITLEDTETRIS_UntitledTetrisGameMode_generated_h
+
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_SPARSE_DATA
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_RPC_WRAPPERS
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_RPC_WRAPPERS_NO_PURE_DECLS
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUntitledTetrisGameMode(); \
+	friend struct Z_Construct_UClass_AUntitledTetrisGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AUntitledTetrisGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UntitledTetris"), UNTITLEDTETRIS_API) \
+	DECLARE_SERIALIZER(AUntitledTetrisGameMode)
+
+
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_INCLASS \
+private: \
+	static void StaticRegisterNativesAUntitledTetrisGameMode(); \
+	friend struct Z_Construct_UClass_AUntitledTetrisGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AUntitledTetrisGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/UntitledTetris"), UNTITLEDTETRIS_API) \
+	DECLARE_SERIALIZER(AUntitledTetrisGameMode)
+
+
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	UNTITLEDTETRIS_API AUntitledTetrisGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUntitledTetrisGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(UNTITLEDTETRIS_API, AUntitledTetrisGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUntitledTetrisGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UNTITLEDTETRIS_API AUntitledTetrisGameMode(AUntitledTetrisGameMode&&); \
+	UNTITLEDTETRIS_API AUntitledTetrisGameMode(const AUntitledTetrisGameMode&); \
+public:
+
+
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UNTITLEDTETRIS_API AUntitledTetrisGameMode(AUntitledTetrisGameMode&&); \
+	UNTITLEDTETRIS_API AUntitledTetrisGameMode(const AUntitledTetrisGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(UNTITLEDTETRIS_API, AUntitledTetrisGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUntitledTetrisGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AUntitledTetrisGameMode)
+
+
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_PRIVATE_PROPERTY_OFFSET
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_15_PROLOG
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_PRIVATE_PROPERTY_OFFSET \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_SPARSE_DATA \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_RPC_WRAPPERS \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_INCLASS \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_PRIVATE_PROPERTY_OFFSET \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_SPARSE_DATA \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_INCLASS_NO_PURE_DECLS \
+	UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h_18_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UNTITLEDTETRIS_API UClass* StaticClass<class AUntitledTetrisGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UntitledTetris_Source_UntitledTetris_UntitledTetrisGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
